970aa20ad90f34c63d8e34bf3b1318585509a80a
Lionel Messi <dperram3009g.educaan.es>
2022-12-12 16:03:07 +0100

    System.out.println("1º DAM B");

--- src/github.java
+++ src/github.java
@@ -3,7 +3,10 @@ public class github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("1º DAM B");
+		String nombre = "Darío Pérez";
+		System.out.println(nombre);
+ 
 	}
 
 }
